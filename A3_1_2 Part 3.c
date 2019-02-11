e305cad28acc50366b697303945cdd4e2467f068
jsp1903 <47366345+jsp1903@users.noreply.github.com>
2019-02-11 12:20:10 -0800

    Update A3_1_2 Part 3.c

--- A3_1_2 Part 3.c
+++ A3_1_2 Part 3.c
@@ -27,16 +27,16 @@ task main()
 {
 
                                       //Program begins, insert code within curly braces
-	startMotor(rightMotor, 64);
+	startMotor(rightMotor, 64); //Spins the motor at half speed for 5 seconds
 	wait(5);
 	stopMotor(rightMotor);
 
-	startMotor(leftMotor, -96);
+	startMotor(leftMotor, -96); //Spins the motor at 3/4 speed for 2.5 seconds in the opposite direction
 	wait(2.5);
 	stopMotor(leftMotor);
 
 
-	startMotor(rightMotor, 127);
+	startMotor(rightMotor, 127); //spins both the motors at the same speed for 7.25 seconds
 	startMotor(leftMotor, 127);
 	wait(7.25);
 	stopMotor(rightMotor);
